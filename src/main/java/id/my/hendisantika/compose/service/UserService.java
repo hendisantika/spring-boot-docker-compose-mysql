a5cc848f7146e0bab34c2c6ae3fb000085b8d843
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 07:08:56 +0700

    Add UserService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/compose/service/UserService.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.compose.service;
+
+import id.my.hendisantika.compose.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-docker-compose-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Visit: https://s.id/hendisantika
+ * Date: 21/05/24
+ * Time: 07.08
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+}
