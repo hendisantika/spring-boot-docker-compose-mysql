6fb2a7bcda1bae7e75a6a921aa7b69928770e193
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 07:02:56 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/compose/SpringBootDockerComposeMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerComposeMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
