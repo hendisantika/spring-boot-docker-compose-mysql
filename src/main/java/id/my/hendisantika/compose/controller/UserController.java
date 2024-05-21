41b9e0c0aa251d1d5d7a59b85e13c113d2a4ca8c
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 07:12:52 +0700

    Add UserController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/compose/controller/UserController.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.compose.controller;
+
+import id.my.hendisantika.compose.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-docker-compose-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Visit: https://s.id/hendisantika
+ * Date: 21/05/24
+ * Time: 07.12
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+}
