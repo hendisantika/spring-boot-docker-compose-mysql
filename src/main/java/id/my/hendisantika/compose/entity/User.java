f916b38e32fc9ebbe6895bcd2951b34af949fdc9
hendisantika <hendisantika@yahoo.co.id>
2024-05-21 07:07:02 +0700

    Add User Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/compose/entity/User.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.compose.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-docker-compose-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Visit: https://s.id/hendisantika
+ * Date: 21/05/24
+ * Time: 07.06
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
